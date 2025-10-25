1ae25e3abe12488501c2947a1a964c8a5387e0d5
MalikaHEDDAJI03 <Malika.Heddaji@emsi-edu.ma>
2025-10-25 09:20:32 +0100

    l'ajout de @Operation

--- src/main/java/com/example/student_management/controllers/StudentController.java
+++ src/main/java/com/example/student_management/controllers/StudentController.java
@@ -2,6 +2,7 @@ package com.example.student_management.controllers;
 
 import com.example.student_management.entity.Student;
 import com.example.student_management.services.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,18 +22,21 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @Operation(summary = "Récupérer tous les étudiants")
     @GetMapping("/students")
     public ResponseEntity<List<Student>> getAllStudents() {
         List<Student> students = studentService.findAll();
         return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
+    @Operation(summary = "Enregistrer un étudiant")
     @PostMapping("/students")
     public ResponseEntity<Student> save(@RequestBody Student student) {
         Student savedStudent = studentService.save(student);
         return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Supprimer un étudiant")
     @DeleteMapping("/students/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") int id) {
         boolean deleted = studentService.delete(id);
@@ -43,12 +47,14 @@ public class StudentController {
         }
     }
 
+    @Operation(summary = "Calculer le nbr d'etudiants")
     @GetMapping("/students/count")
     public ResponseEntity<Long> countStudent() {
         long count = studentService.countStudents();
         return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
+    @Operation(summary = "Le nbr d'etudiant par annee de naissance")
     @GetMapping("/students/byYear")
     public ResponseEntity<Collection<?>> findByYear() {
         Collection<?> studentsByYear = studentService.findNbrStudentByYear();
